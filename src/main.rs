af834603a1a138053468e2e83d232696b4c18f7c
negative-seven <21268649+negative-seven@users.noreply.github.com>
2023-05-01 02:27:13 +0200

    Add tracing

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,17 @@
-use std::error::Error;
-
 mod api;
 mod root;
 mod server_state;
 mod token;
 
+use std::error::Error;
+use tracing_subscriber::EnvFilter;
+
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
+    tracing_subscriber::fmt()
+        .with_env_filter(EnvFilter::from_default_env())
+        .init();
+
     axum::Server::bind(&"0.0.0.0:3000".parse()?)
         .serve(root::create_router().into_make_service())
         .await?;
--- src/root.rs
+++ src/root.rs
@@ -1,10 +1,11 @@
-use std::sync::{Arc, Mutex};
-
 use crate::{api, server_state::ServerState};
 use axum::Router;
+use std::sync::{Arc, Mutex};
+use tower_http::trace::TraceLayer;
 
 pub fn create_router() -> Router {
     Router::new()
         .nest("/api", api::create_router())
+        .layer(TraceLayer::new_for_http())
         .with_state(Arc::new(Mutex::new(ServerState::new())))
 }
